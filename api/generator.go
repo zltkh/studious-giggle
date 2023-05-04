bf05564bc733465e4f6f41626d6d6fb1f2191d94
zltkh <109478650+zltkh@users.noreply.github.com>
2023-05-05 00:29:48 +0300

    Update generator.go

--- api/generator.go
+++ api/generator.go
@@ -20,16 +20,39 @@ func RandStringRunes(n int) string {
 }
 
 
-func writeToDB(token, prompt string) error {
+func writeToDB(token, prompt, num_beams, temperature, top_p, num_beam_groups string) error {
     db, err := sql.Open("mysql", "l905412p_project:4V6NR5Cg@tcp(l905412p.beget.tech)/l905412p_project")
     if err != nil {
         return err
     }
     defer db.Close()
-    _, err = db.Exec("INSERT INTO requests(token, prompt) VALUES(?, ?)", token, prompt)
+    _, err = db.Exec("INSERT INTO requests(token, prompt, num_beams, temperature, top_p, num_beam_groups) VALUES(?, ?, ?, ?, ?, ?)", token, prompt, num_beams, temperature, top_p, num_beam_groups)
     return err 
 }
 
+func isPositiveInteger(s string) bool {
+    n, err := strconv.Atoi(s)
+    if err != nil || n <= 0 {
+        return false
+    }
+    return true
+}
+
+func isPositiveNumber(s string) bool {
+    num, err := strconv.ParseFloat(s, 64)
+    if err != nil || num <= 0 {
+        return false
+    }
+    return true
+}
+
+func isPositiveNumber2(s string) bool {
+    num, err := strconv.ParseFloat(s, 64)
+    if err != nil || num <= 0 || num > 1 {
+        return false
+    }
+    return true
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Access-Control-Allow-Origin", "*") // Разрешение запросов со всех источников
@@ -40,18 +63,61 @@ func Handler(w http.ResponseWriter, r *http.Request) {
         return
     }
     prompt := r.FormValue("prompt")
+    num_beams := r.FormValue("num_beams")
+    temperature := r.FormValue("temperature")
+    top_p := r.FormValue("top_p")
+    num_beam_groups := r.FormValue("num_beam_groups")
+
     token := RandStringRunes(32)
+    
     if len(prompt) > 400 {
         http.Error(w, "Prompt is too large. Max length is 400 symbols.", http.StatusBadRequest)
         return
     }
+	if len(num_beams) == 0 {
+		num_beams = "4"
+	}
+	
+	if len(temperature) == 0 {
+		temperature = "1"
+	}
+	
+	if len(top_p) == 0 {
+		num_beams = "0.8"
+	}
+	
+	if len(num_beam_groups) == 0 {
+		num_beam_groups = "4"
+	}
+	
+	if !isPositiveInteger(num_beams) {
+		http.Error(w, "num_beams should be positive integer", http.StatusBadRequest)
+        	return
+	}
+	
+	if !isPositiveInteger(num_beam_groups) {
+		http.Error(w, "num_beam_group should be positive integer", http.StatusBadRequest)
+        	return
+	}
+	
+	if !isPositiveNumber(temperature) {
+		http.Error(w, "temperature should be positive number", http.StatusBadRequest)
+        	return
+	}
+	
+	if !isPositiveNumber(top_p) {
+		http.Error(w, "top_p should be positive number greater than 0 and less then 1", http.StatusBadRequest)
+        	return
+	}
+	
+	
 
     if len(prompt) == 0 {
         http.Error(w, "Promp must be specified", http.StatusBadRequest)
         return
     }
 
-    if err := writeToDB(token, prompt); err != nil {
+    if err := writeToDB(token, prompt, num_beams, temperature, top_p, num_beam_groups); err != nil {
         http.Error(w, fmt.Sprintf("Internal error: %s", err), http.StatusInternalServerError)
         return
     }
